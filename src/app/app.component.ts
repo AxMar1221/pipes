fcaae13febe09fa25138d7fea483a757fc9e8315
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-19 14:05:59 -0500

    slice 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre: string = 'Bart Simpson'
+  nombre: string = 'Bart Simpson';
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10]
 }
