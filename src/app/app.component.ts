f5cb4e4c9cce7c3ef9ba186c0c4d5734739212c1
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-19 14:16:10 -0500

    number/decimal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre: string = 'Bart Simpson';
 
-  arreglo = [1,2,3,4,5,6,7,8,9,10]
+  personajes: string[] = ['Homero', ' Marge',' Bart', ' Lisa', ' Maguie', ' Apu', ' Nelson', ' Flanders', ' Milhouse']
+  PI: number = Math.PI
 }
