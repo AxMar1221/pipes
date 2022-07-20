d3775577364ba19bd7a5f0e93404afa417e97409
Tachi Dito <tachidito_7inc@hotmail.com>
2022-07-19 22:57:30 -0500

    idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { registerLocaleData } from '@angular/common';
 import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
 
 registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 import { AppComponent } from './app.component';
 
